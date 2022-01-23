3409bbfd5475f25eaa8cc578b0306660e1be389f
gtqifan <qifany1111@gmail.com>
2022-01-23 14:03:00 -0500

    Add user account APIs

--- server.js
+++ server.js
@@ -25,19 +25,98 @@ const server = app.listen(5000, () => {
 });
 
 // The method for connection check. Will be implemented so that an index page will be returned.
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     sql.connect(sqlConfig, () => {
         res.send("Connection established");
     });
 });
 
+// RESTful API for adding new users.
+app.post('/users/add', (req, res) => {
+    const userName = req.body.userName;
+    const pwd = req.body.pwd;
+    sql.connect(sqlConfig, () => {
+        const request = new sql.Request();
+        const stringCheck = `SELECT * FROM Account WHERE userName = '${userName}'`; // check if the username is used
+        request.query(stringCheck, (err, response) => {
+            if(err) {
+                console.log(err);
+            }
+            if(response.rowsAffected[0] === 0) { // check username and count the number of queries in the database
+                const stringRequest = `INSERT INTO Users.dbo.Account (userName, pwd) VALUES ('${userName}', '${pwd}')`;
+                request.query(stringRequest, (err, response) => {
+                    if(err) {
+                        console.log(err);
+                    }
+                });
+                res.send('success'); // return success if the new user is added to the database
+            } else {
+                res.send('The username is already used.');
+            }
+        });
+    });
+});
+
+// RESTful API for login and verification.
+app.post('/users/verify', (req, res) => {
+    const userName = req.body.userName;
+    const pwd = req.body.pwd;
+    sql.connect(sqlConfig, () => {
+        const request = new sql.Request();
+        const stringCheck = `SELECT * FROM Account WHERE userName = '${userName}'`; // retrieve the pwd info
+        request.query(stringCheck, (err, response) => {
+            if(err) {
+                console.log(err);
+            }
+            // check if username exists in the database and then the password matches
+            if(response.rowsAffected[0] === 1 && response.recordsets[0][0].pwd === pwd) {
+                if(response.recordsets)
+                res.send('success'); // return success if the new user is added to the database
+            } else {
+                res.send('fail');
+            }
+        });
+    });
+});
+
+// RESTful API for login and verification.
+app.post('/users/update', (req, res) => {
+    const userName = req.body.userName;
+    const oldPwd = req.body.oldPwd;
+    const newPwd = req.body.newPwd;
+    sql.connect(sqlConfig, () => {
+        const request = new sql.Request();
+        const stringCheck = `SELECT * FROM Account WHERE userName = '${userName}'`; // retrieve the pwd info
+        request.query(stringCheck, (err, response) => {
+            if(err) {
+                console.log(err);
+            }
+            // check if username exists in the database and then the password matches
+            if(response.rowsAffected[0] === 1 && response.recordsets[0][0].pwd === oldPwd) {
+                const stringUpdate = `INSERT INTO Users.dbo.Account (userName, pwd) VALUES ('${userName}', '${newPwd}')`;
+                request.query(stringUpdate, (err, response) => {
+                    if(err) {
+                        console.log(err);
+                    }
+                });
+                res.send('success');
+            } else {
+                res.send('fail');
+            }
+        });
+    });
+});
+
 // RESTful API interface for retrieving all username and password on the server. Will be modified so that it will
 // only return a boolean indicating the correctness of account info.
-app.get("/users", (req, res) => {
+// TODO: remove this function because this is only for testing
+app.get('/users', (req, res) => {
     sql.connect(sqlConfig, () => {
         const request = new sql.Request();
         request.query('SELECT * FROM Account', (err, recordset) => {
-            if(err) console.log(err);
+            if(err) {
+                console.log(err);
+            }
             res.send(JSON.stringify(recordset));
         });
     });
@@ -47,22 +126,13 @@ app.get("/users", (req, res) => {
 app.get('/users/:uid/', (req, res) => {
     sql.connect(sqlConfig, () => {
         const request = new sql.Request();
-        const stringRequest = 'SELECT * FROM Account WHERE uid = ' + req.params.uid;
+        const stringRequest = `SELECT * FROM Account WHERE uid = ${req.params.uid}`;
         request.query(stringRequest, function(err, recordset) {
-            if(err) console.log(err);
+            if(err) {
+                console.log(err);
+            }
             res.send(JSON.stringify(recordset)); // Result in JSON format
         });
     });
 });
 
-// RESTful API for adding new users.
-// app.post("/users/add", (req, res) => {
-//     const userName = req.body.userName;
-//     const pwd = req.body.pwd;
-//     const request = new sql.Request();
-//     request.query(`INSERT Account (userName, pwd) VALUES (${userName}, ${pwd})`, (err, res) => {
-//         if(err) {
-//             console.log(err);
-//         }
-//     });
-// });
