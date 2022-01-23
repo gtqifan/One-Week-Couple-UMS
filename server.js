d59ad6021c1d3cbc883365a32fd5f016546a1b26
gtqifan <qifany1111@gmail.com>
2022-01-23 03:31:17 -0500

    Add comments

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const sql = require("mssql");
 const app = express();
 app.use(bodyParser.json());
 
+// This is the basic authentication for MSSQL database
 const sqlConfig = {
     user: 'foo',
     password: 'foo', //TODO: need to be changed here
@@ -16,18 +17,22 @@ const sqlConfig = {
     }
 };
 
+// The server runs on port 5000. Can be accessed with 'localhost:5000'
 const server = app.listen(5000, () => {
     const port = server.address().port;
 
     console.log("app is listening at port %s", port);
 });
 
+// The method for connection check. Will be implemented so that an index page will be returned.
 app.get("/", (req, res) => {
     sql.connect(sqlConfig, () => {
         res.send("Connection established");
     });
 });
 
+// RESTful API interface for retrieving all username and password on the server. Will be modified so that it will
+// only return a boolean indicating the correctness of account info.
 app.get("/users", (req, res) => {
     sql.connect(sqlConfig, () => {
         const request = new sql.Request();
@@ -38,6 +43,7 @@ app.get("/users", (req, res) => {
     });
 });
 
+// RESTful API interface for retrieving one user account info with the unique uid.
 app.get('/users/:uid/', (req, res) => {
     sql.connect(sqlConfig, () => {
         const request = new sql.Request();
@@ -49,6 +55,7 @@ app.get('/users/:uid/', (req, res) => {
     });
 });
 
+// RESTful API for adding new users.
 // app.post("/users/add", (req, res) => {
 //     const userName = req.body.userName;
 //     const pwd = req.body.pwd;
