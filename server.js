85d2fdb80ab4955f92c79222b304f199b243e240
gtqifan <qifany1111@gmail.com>
2022-02-03 15:36:48 -0500

    roll back header setting

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 
 // Setup return header
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", req.headers.origin ? req.headers.origin : "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, cache-control, Access-Control-Request-Method, Access-Control-Request-Headers");
